8b02ba62466b5d48e4af5627bc01513be435496e
arian <arianpourinfo@gmail.com>
2015-08-12 14:35:46 +0800

    layout.master'

---
+++ resources/views/apps/contacts/addContact.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+@section('title','Add New contact')
+
+@section('page-head')
+
+
+@endsection
+
+@section('body-class','app-contacts')
+
+		<!-- Page -->
+@section('content')
+@section('div-class','page bg-white animsition')
+
+
+
+@endsection
